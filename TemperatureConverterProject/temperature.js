4501a85c95366b62798b82ef730917091e9026a1
David Watkins <david.watkins457@west-mec.org>
2016-10-26 15:18:37 -0700

    update

--- TemperatureConverterProject/temperature.js
+++ TemperatureConverterProject/temperature.js
@@ -1,10 +1,38 @@
-function calculate(degrees){
+"use strict";
+function calculate(degrees) {
+    var temp = document.getElementsByClassName("temp");
     var temperature;
-    if(degrees  == "f"){
-    temperature = document.getElementById("fahrenheit").value * (9/5) + 32;
-            document.getElementById("celsius").value = Math.round(temperature)}
-        else {
-            temperature =  (document.getElementById("celsius").value -32) * (5/9);
-        document.getElementById("fahrenheit").value = Math.round(temperature);
+    var fahrenheit = document.getElementById("f");
+    var celsius = document.getElementById("c");
+    if (degrees === "f") {
+        temperature = (fahrenheit.value - 32) * (5 / 9);
+        celsius.value = temperature.toFixed(2);
+
+    }
+    else {
+        temperature = celsius.value * (9 / 5) + 32;
+        fahrenheit.value = temperature.toFixed(2);
+
+    } //Ending of a number
+    // if(isNaN(temp) || ""){
+    //  alert("Please enter a numeric value");
+    // return false;
+// Loop
+    for (temp; isNaN(temp) || ""; ) {
+        alert ("Please enter a numeric value");
+        break;
+    }
+
+    try {
+
+        if (fahrenheit == "") {
+            fahrenheit.value = "0";
+
+        document.getElementById("f").value = NaN;
         }
-}
+    }
+    catch (err) {
+        console.log(err);
+    }
+}// Ending of function
+
